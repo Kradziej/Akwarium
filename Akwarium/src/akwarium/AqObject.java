ca68c1914fb450400155cdaee3eb4428aa41bd7e
1 <1@1-Komputer>
2014-08-01 11:04:17 +0200

    aaa

--- Akwarium/src/akwarium/AqObject.java
+++ Akwarium/src/akwarium/AqObject.java
@@ -2,12 +2,12 @@ package akwarium;
 
 import java.awt.image.BufferedImage;
 
-public abstract class AqObject {
+public abstract class AqObject implements Runnable {
 	
 	protected int x;
 	protected int y;
-	protected float[] vector = {0,0};
-	protected int direction = 1;
+	protected float[] vector = {0,0};     //put direction here
+	//protected int direction = 1;
 	protected int v;   // pixels per sec
 	protected int imageWidth;
 	protected int imageHeight;
---
+++ Akwarium/src/akwarium/Swim.java
@@ -0,0 +1,7 @@
+package akwarium;
+
+public interface Swim extends Runnable {
+	
+	
+
+}
