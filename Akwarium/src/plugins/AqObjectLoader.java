d05a5e0b400e8db2c1ae80245b67f8afd7395705
1 <1@1-Komputer>
2014-08-14 16:17:12 +0200

    object loader

--- Akwarium/src/plugins/AqObjectLoader.java
+++ Akwarium/src/plugins/AqObjectLoader.java
@@ -1,8 +1,22 @@
 package plugins;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.ToolProvider;
 
 import akwarium.Program;
 
@@ -11,7 +25,7 @@ public class AqObjectLoader {
 	private String pluginFolderName = "plugins";
 	
 	// located in new directory where jar file is located 
-	public void getJavaFiles () {
+	public Path getJavaFiles () {
 		
 		URL jarLocationURL = Program.class.getProtectionDomain().getCodeSource().getLocation();
 		File jarFile = null;
@@ -21,11 +35,39 @@ public class AqObjectLoader {
 		} catch (URISyntaxException e) {
 			System.out.println("Incorrect path to plugins: " + jarLocationURL.toString());
 		}
-		
-		if(jarFile.isFile()) {
-			
-			jarFile.getParentFile().listFiles();
+	
+		Path pluginFolder =  Paths.get(jarFile.getParentFile().getAbsolutePath(), pluginFolderName);
+		if(pluginFolder.toFile().isDirectory()) {
+				
+			System.out.println("Plugins folder doesn't exist!");
+			return null;
 		}
+		
+		return pluginFolder;
+		
 	}
+	
+	
+	public void compilePlugins(Iterable<SimpleJavaFileObject> files) {
+		
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
+		
+		CompilationTask task = compiler.getTask(null, null, diagnostics, null, null, files);
+		boolean result = task.call();
+		
+		if(!result) {
+			
+			System.out.println("Plugins compilation failed");
+			for(Diagnostic diag : diagnostics.getDiagnostics()) {
+				
+				System.out.println(diag.getKind().toString() + ": Line " + diag.getLineNumber());
+				System.out.println(diag.getMessage(null));
+			}
+		}
+		
+	}
+	
+	public void loadPlugins() {}
 
 }
